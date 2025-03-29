b50d8b523d6fb01e2748831c4ceec450b4e7b6dd
Tyreece Rozycki <57762380+KrystilizeNevaDies@users.noreply.github.com>
2025-03-29 21:25:45 +1000

    new project for json schema based class generation

---
+++ backend/FizzBuzzService.Generation/Program.cs
@@ -0,0 +1,10 @@
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration.TypeScript;
+
+// var schema = JsonSchema.FromType<Game>();
+var schema = await JsonSchema.FromFileAsync("gameSchema.json");
+
+var generator = new TypeScriptGenerator(schema);
+var source = generator.GenerateFile();
+
+Console.WriteLine(source);
